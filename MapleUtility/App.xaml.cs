dbafd9a8f51acbdd840d1229bc1ff35a0dcdec69
Sparrkle <twilymoomoo@gmail.com>
2023-08-31 18:21:24 +0900

    Singleton 구현

--- MapleUtility/App.xaml.cs
+++ MapleUtility/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using FMUtils.KeyboardHook;
 using MapleUtility.Plugins.Models;
+using MapleUtility.Plugins.Views.Windows;
+using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Forms;
 using Telerik.Windows.Controls;
@@ -14,6 +17,58 @@ namespace MapleUtility
         public static Hook KeyboardHook = new Hook("Global Keyboard Hook");
         public static BlockManagerItem BlockManager = new BlockManagerItem();
         internal static NotifyIcon ni = new NotifyIcon();
+
+        #region Constants and Fields
+
+        /// <summary>The event mutex name.</summary>
+        private const string UniqueEventName = "{4566FA72-DB67-44DC-96D0-8B817426A0C3}";
+
+        /// <summary>The unique mutex name.</summary>
+        private const string UniqueMutexName = "{66889730-06FC-4177-9CAD-D7839A6A8A24}";
+
+        /// <summary>The event wait handle.</summary>
+        private EventWaitHandle eventWaitHandle;
+
+        /// <summary>The mutex.</summary>
+        private Mutex mutex;
+
+        #endregion
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool isOwned;
+            this.mutex = new Mutex(true, UniqueMutexName, out isOwned);
+            this.eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            // So, R# would not give a warning that this variable is not used.
+            GC.KeepAlive(this.mutex);
+
+            if (isOwned)
+            {
+                // Spawn a thread which will be waiting for our event
+                var thread = new Thread(
+                    () =>
+                    {
+                        while (this.eventWaitHandle.WaitOne())
+                        {
+                            Current.Dispatcher.BeginInvoke(
+                                (Action)(() => ((WindowMain)Current.MainWindow).BringToForeground()));
+                        }
+                    });
+
+                // It is important mark it as background otherwise it will prevent app from exiting.
+                thread.IsBackground = true;
+
+                thread.Start();
+                return;
+            }
+
+            // Notify other instance so it could bring itself to foreground.
+            this.eventWaitHandle.Set();
+
+            // Terminate this instance.
+            this.Shutdown();
+        }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
--- MapleUtility/Plugins/Views/Windows/WindowMain.xaml.cs
+++ MapleUtility/Plugins/Views/Windows/WindowMain.xaml.cs
@@ -294,5 +294,20 @@ namespace MapleUtility.Plugins.Views.Windows
 
             base.OnStateChanged(e);
         }
+
+        public void BringToForeground()
+        {
+            if (this.WindowState == WindowState.Minimized || this.Visibility == Visibility.Hidden)
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+            }
+
+            // According to some sources these steps gurantee that an app will be brought to foreground.
+            this.Activate();
+            this.Topmost = true;
+            this.Topmost = false;
+            this.Focus();
+        }
     }
 }
