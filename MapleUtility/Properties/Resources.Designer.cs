9e30e45f8b0366db0b325fb27b04b337ae6794cc
Sparrkle <twilymoomoo@gmail.com>
2025-04-09 00:03:54 +0900

    이미지 변경

--- MapleUtility/Properties/Resources.Designer.cs
+++ MapleUtility/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace MapleUtility.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap churuko_image {
+            get {
+                object obj = ResourceManager.GetObject("churuko_image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap churuko_white {
             get {
                 object obj = ResourceManager.GetObject("churuko_white", resourceCulture);
